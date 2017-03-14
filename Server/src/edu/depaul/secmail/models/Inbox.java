6e10816babbadc43b6f68ed8c81b19e0a5453258
wpank <will.pankiewicz@gmail.com>
2017-03-14 15:55:30 -0500

    Added to inbox.java.Implemented retrieving tags from a DBNotification

--- Server/src/edu/depaul/secmail/models/Inbox.java
+++ Server/src/edu/depaul/secmail/models/Inbox.java
@@ -7,7 +7,7 @@ import edu.depaul.secmail.Notification;
 
 public class Inbox {
 	
-	private ArrayList<Notification> inbox = new ArrayList<Notification>();
+	private ArrayList<DBNotification> inbox = new ArrayList<DBNotification>();
 	private User user;
 	
 	public Inbox(){
@@ -15,30 +15,34 @@ public class Inbox {
 	}
 	
 	
-	
-	public void addMessage(Message message){
-		//inbox.add(message);
+	// gets a message from a notification
+	public void getMessageFromNotification(DBNotification notification){
+		String sql = "SELECT * FROM notification WHERE message_id = "+ notification.getEmailID() + " AND user_id = "+ user.getID() + ";";
+		inbox.add(DBNotification.getNotificationFromSQLStatement(sql));
 	}
 	
-	public Notification getMessage(int index){
+	// Returns a notification by its index
+	public DBNotification getNotificationByIndex(int index){
 		return inbox.get(index);
 	}
 	
-	public boolean hasMessageWithID(int messageID){
-		//for (Message message : inbox){
-		//	if (message.getID() == messageID){
-		//		return true;
-		//	}
-		//}
+	// Boolean that tests if an inbox has a message with an id
+	public boolean hasMessageWithID(String messageID){
+		for (DBNotification notification : inbox){
+			if (notification.getEmailID() == messageID){
+				return true;
+			}
+		}
 		return false;
 	}
 	
-	public Message findMessageByID(int messageID){
+	// returns a DBNotification in an inbox by its index in the arraylist
+	public DBNotification findMessageByID(String messageID){
 		if (hasMessageWithID(messageID)){
-			for (Notification message : inbox){
-				//if (message.getID() == messageID){
-				//	return message;
-				//}
+			for (DBNotification message : inbox){
+				if (message.getEmailID() == messageID){
+					return message;
+				}
 			}
 		} else {
 			throw new IllegalArgumentException();
@@ -46,14 +50,25 @@ public class Inbox {
 		return null;
 	}
 	
-	public ArrayList<Message> getMessagesWithTag(Tag tag){
-		ArrayList<Message> taggedMessages = new ArrayList<Message>();
-		//for (Message message: inbox){
-		//	if (message.getTags().contains(tag)){
-		//		taggedMessages.add(message);
-		//	}
-		//}
+	// Returns an arraylist of DBNotifications in an inbox with a given tag
+	public ArrayList<DBNotification> getDBNotificationsWithTag(Tag tag){
+		ArrayList<DBNotification> taggedMessages = new ArrayList<DBNotification>();
+		for (DBNotification notification: inbox){
+			if (notification.getTagsFromDBNotification().contains(tag)){
+				taggedMessages.add(notification);
+			}
+		}
 		return taggedMessages;
 	}
 
+	// Returns an arraylist of Messages in an inbox with a given tag
+	public ArrayList<Message> getMessagessWithTag(Tag tag){
+		ArrayList<Message> taggedMessages = new ArrayList<Message>();
+		for (DBNotification notification: inbox){
+			if (notification.getTagsFromDBNotification().contains(tag)){
+				taggedMessages.add(notification.getMessageByDBNotification());
+			}
+		}
+		return taggedMessages;
+	}
 }
